4c8d742506175c151c431aba8b0512299cd91eb0
Cameron Robinson <robinson.cameron90@gmail.com>
2016-07-13 18:26:28 -0400

    new main site

---
+++ Scripts/Paralax.js
@@ -0,0 +1,17 @@
+﻿
+//for material box in portfolio page
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+    $('.collapsible').collapsible({
+        accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+});
+
+//Fade pages
+$(function () {
+    $('#divFade').removeClass('fade-out');
+});
+
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
